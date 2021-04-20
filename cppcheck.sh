ea30e890bee9a66acd742d4c5c51cb6261af2de1
linciclon <a82273@alunos.uminho.pt>
2021-04-20 16:57:30 +0100

    first commit

---
+++ cppcheck.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export NEW_DIR=$(pwd)
+
+shopt -s nullglob dotglob     # To CHECK IF THERE IS ANY FILE IN SRC FILE
+files=($DIR *)
+if [ ${#files[@]} -gt 0 ]
+then 
+  echo "files:"
+  cppcheck --error-exitcode=1 --dump *.c
+  python /home/joao/cppcheck-donate-cpu-workfolder/cppcheck/addons/misra.py --rule-texts=./rules.txt *.c.dump
+else
+  echo "THERE IS NO FILES IN THIS DIR" 
+fi
