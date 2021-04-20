1c85126d4482c7f42744adaa4a0e3be92bbc45a8
linciclon <a82273@alunos.uminho.pt>
2021-04-20 18:07:23 +0100

    test cppce

--- travis_cppcheck.sh
+++ travis_cppcheck.sh
@@ -47,7 +47,7 @@ then
   cppcheck --error-exitcode=1 --dump *.c
   python /home/joao/cppcheck-donate-cpu-workfolder/cppcheck/addons/misra.py --rule-texts=./rules.txt *.c.dump
   cd $DIR_ARCH_ARM/inc/arch/
-  cppcheck --error-exitcode=1 *.c
+  #cppcheck --error-exitcode=1 *.c
 else
   echo "THERE IS NO FILES IN THIS DIR" 
 fi
