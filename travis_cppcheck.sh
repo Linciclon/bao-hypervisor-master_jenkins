182e73ccde313159c2165e53a2eb3b6b8497b054
linciclon <a82273@alunos.uminho.pt>
2021-04-20 18:03:22 +0100

    teste diretoria misra

--- travis_cppcheck.sh
+++ travis_cppcheck.sh
@@ -44,7 +44,8 @@ files=($DIR *)
 if [ ${#files[@]} -gt 0 ]
 then 
   echo "files:"
-  cppcheck --error-exitcode=1 *.c
+  cppcheck --error-exitcode=1 --dump *.c
+  python /home/joao/cppcheck-donate-cpu-workfolder/cppcheck/addons/misra.py --rule-texts=./rules.txt *.c.dump
   cd $DIR_ARCH_ARM/inc/arch/
   cppcheck --error-exitcode=1 *.c
 else
