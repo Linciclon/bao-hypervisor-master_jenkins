a4e70343e3401ab2a2831456d7b15ad519419444
linciclon <a82273@alunos.uminho.pt>
2021-04-20 18:26:45 +0100

    MISRA

--- travis_cppcheck.sh
+++ travis_cppcheck.sh
@@ -45,7 +45,7 @@ if [ ${#files[@]} -gt 0 ]
 then 
   echo "files:"
   cppcheck --error-exitcode=1 --dump *.c
-  python /home/joao/cppcheck-donate-cpu-workfolder/cppcheck/addons/misra.py --rule-texts=./rules.txt *.c.dump
+  python $BASEDIR/misra.py --rule-texts=./rules.txt *.c.dump
   ls
   cd $DIR_ARCH_ARM/inc/arch/
   echo MISRA
